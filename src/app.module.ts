0de64b4d9e869b630a4e104a03772bff682b9843
ayman <aymanfa577@gmail.com>
2024-10-25 12:17:28 +0100

    adding the database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'mssql_db',
+      port: 1433,
+      username: 'sa',
+      password: '732317',
+      database: 'nestjs_docker_db',
+      entities: [],
+      synchronize: true,
+      options: {
+        encrypt: true,
+        trustServerCertificate: true,
+        enableArithAbort: true,
+        cryptoCredentialsDetails: {
+          minVersion: 'TLSv1'
+        }
+      },
+      extra: {
+        trustServerCertificate: true,
+        encrypt: true
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): any {
-    return { msg: 'Hello World!' };
+    return { msg: 'Hello World!!!!' };
   }
 }
